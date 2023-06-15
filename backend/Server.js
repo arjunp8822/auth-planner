a1af1aaf110b9969fbd7f9a7c8c7c0d19f871316
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-06-15 19:14:30 +1000

    added frontend react

--- backend/Server.js
+++ backend/Server.js
@@ -18,50 +18,70 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, res) => {
-  const { username, password, confirmPassword } = req.body;
-  if (!username || !password || !confirmPassword) {
-    return res.status(400).json({ message: "Enter all required fields" });
+  try {
+    const { username, password, confirmPassword } = req.body;
+    if (!username || !password || !confirmPassword) {
+      return res.status(400).json({ message: "Enter all required fields" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be 6 or more characters" });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+    const existingUser = await User.find({ username: username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const userData = {
+      username: username,
+      password: await bcrypt.hash(password, 10),
+    };
+    const user = new User({
+      username: userData.username,
+      password: userData.password,
+    });
+    const savedUser = await user.save();
+    res.status(200).json({ message: "User registered" });
+  } catch (e) {
+    res.status(500).json({ message: e });
   }
-  if (password.length < 6) {
-    return res
-      .status(400)
-      .json({ message: "Password must be 6 or more characters" });
-  }
-  if (password !== confirmPassword) {
-    return res.status(400).json({ message: "Passwords do not match" });
-  }
-  const existingUser = await User.find({ username: username });
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exists" });
-  }
-  const userData = {
-    username: username,
-    password: await bcrypt.hash(password, 10),
-  };
-  const user = new User({
-    username: userData.username,
-    password: userData.password,
-  });
-  const savedUser = await user.save();
-  res.status(200).json({ message: "User registered" });
 });
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ message: "Enter all required fields" });
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Enter all required fields" });
+    }
+    const existingUser = await User.find({ username: username });
+    if (existingUser.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Username or password incorrect" });
+    }
+    const verifiedUser = await bcrypt.compare(
+      password,
+      existingUser[0].password
+    );
+    if (!verifiedUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or password incorrect" });
+    }
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      process.env.JWT_SECRET
+    );
+    res.cookie("token", token);
+    res.status(200).json({ message: "User logged in" });
+  } catch (e) {
+    res.status(500).json({ message: e });
   }
-
-  const userData = {
-    username: username,
-    password: await bcrypt.hash(password, 10),
-  };
-  const user = new User({
-    username: userData.username,
-    password: userData.password,
-  });
-  const savedUser = await user.save();
-  res.status(200).json({ message: "User registered" });
 });
 
 app.listen(4000);
